0056b6201ed76cd0fe2d6ddf5f1657be579bb3d0
KonkenBonken <konrad05pettersson@gmail.com>
2022-05-16 19:10:15 +0200

    Moved commands to own file

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ import Import_discordOauth2 from "discord-oauth2";
 import Import_topGgSdk from '@top-gg/sdk';
 import Import_nodeCache from 'node-cache';
 // import Import_emojiTestList from 'emoji-test-list';
+import Import_commands from './lib/commands.js';
 import EmojiList from './src/emojis.js';
 console.timeEnd('Packages');
 console.time('Consts');
@@ -218,6 +219,9 @@ const NewGuildSubrcibers = [],
 			.Append(set && newDiv().Attribute('list', set))
 			.Append(Select.Role({ guild }))
 	};
+
+const commands = Import_commands({ client, DataBase, Cache, WriteDataBase });
+
 const RandomUser = () => 'User#' + Math.floor(Math.random() * 8999 + 1000),
 	// Sass = file => new Promise((resolver, reject) => sass.render({
 	// 	file: `client/${file}.scss`
@@ -2369,171 +2373,6 @@ const Page = {
 			if (dataBaseGuild.levels.isEmpty()) dataBaseGuild.levels = undefined;
 		}),
 	},
-	argEl = (arg, des) => `<span class="argel" arg="${arg}"><span>${des}</span></span>`,
-	durationDescription = 'possible units are d, h, m, s; example: 2h30m = 2 hours and 30 minutes; max 21600 seconds',
-	commands = [
-		/*{
-			com: 'adduser',
-			des: 'Used by staff to Add a user to a Support Channel',
-			beUsed: 'in support channels if enabled', //  "Can be used " + beUsed  -  default = 'anywhere'
-			format: argEl('@user') // prefix + command + format
-		}, {
-			com: 'removeuser',
-			des: 'Used by staff to Remove a user from a Support Channel',
-			beUsed: 'in support channels if enabled',
-			format: argEl('@user')
-		},*/
-		{
-			com: 'suggest',
-			des: 'Used by member to submit a Suggestion',
-			// beUsed: 'in the suggestion channels if enabled',
-			format: argEl('suggestion', 'text; max 4096 characters'),
-			options: {
-				index: { type: 'INTEGER', description: 'The index of the suggestion', minValue: 1, required: true },
-				suggestion: { type: 'STRING', description: 'Your suggestion', required: true }
-			}
-		}, {
-			com: 'approve',
-			des: 'Used by moderator to approve a Suggestion',
-			// beUsed: 'in the suggestion channels if enabled',
-			format: argEl('index', 'number') + argEl('reason', 'text; max 1024 characters'),
-			options: {
-				index: { type: 'INTEGER', description: 'The index of the suggestion', minValue: 1, required: true },
-				reason: { type: 'STRING', description: 'Why you approve this suggestion', required: true }
-			}
-		}, {
-			com: 'deny',
-			des: 'Used by moderator to deny a Suggestion',
-			// beUsed: 'in the suggestion channels if enabled',
-			format: argEl('index', 'number') + argEl('reason', 'text; max 1024 characters'),
-			options: {
-				index: { type: 'INTEGER', description: 'The index of the suggestion', minValue: 1, required: true },
-				reason: { type: 'STRING', description: 'Why you deny this suggestion', required: true }
-			}
-		}, {
-			com: 'consider',
-			des: 'Used by moderator to consider a Suggestion',
-			// beUsed: 'in the suggestion channels if enabled',
-			format: argEl('index', 'number') + argEl('reason', 'text; max 1024 characters'),
-			options: {
-				index: { type: 'INTEGER', description: 'The index of the suggestion', minValue: 1, required: true },
-				reason: { type: 'STRING', description: 'Why you consider this suggestion', required: true }
-			}
-		}, {
-			com: 'echo',
-			des: 'Used by admin to send message through the bot; The bot copies what you say',
-			format: argEl('message', 'text; max 4000 characters'),
-			options: {
-				message: { type: 'STRING', description: 'The message you want to send', required: true }
-			}
-		}, {
-			com: 'slowmode',
-			des: 'Used by moderator to set the slowmode of the current channel; Use command without arguments to disable slowmode',
-			format: argEl('?duration', `duration; ${durationDescription}; optional`),
-			options: {
-				duration: { type: 'STRING', description: `${durationDescription}; default: unlimited`, required: false }
-			}
-		}, {
-			com: 'clear',
-			des: 'Used by moderator to delete an amount of messages in the current channel',
-			format: argEl('amount', 'number; max 499'),
-			options: {
-				amount: { type: 'INTEGER', description: 'Amount of messages to remove', minValue: 1, maxValue: 499, required: true }
-			}
-		},
-
-		{
-			com: 'info user',
-			des: 'Used by moderator to get info about a member',
-			format: argEl('@user', 'user-tag or id'),
-			options: {
-				user: { type: 'USER', description: 'The user you want the information of', required: true }
-			}
-		}, {
-			com: 'info server',
-			des: 'Used by moderator to get info about the current server',
-			// format:
-		}, {
-			com: 'info role',
-			des: 'Used by moderator to get info about a role',
-			format: argEl('@role', 'role-tag or id'),
-			options: {
-				role: { type: 'ROLE', description: 'The role you want the information of', required: true }
-			}
-		},
-
-		{
-			com: 'infractions',
-			des: 'Used by moderator to get a member\'s last logged infractions',
-			format: argEl('@user', 'user-tag or id'),
-			options: {
-				user: { type: 'USER', description: 'The user you want the infractions from; default: you', required: false },
-				amount: { type: 'INTEGER', description: 'The amount of infractions return; default: 10', required: false }
-			}
-		}, {
-			com: 'warn',
-			des: 'Used by command-specific role or admin to warn a member',
-			format: argEl('@user', 'user-tag or id') + argEl('?reason', 'text; max 512 characters'), //ban reason is limiting, otherwise 512
-			txt1: 'warn',
-			txt: 'warned',
-			options: {
-				user: { type: 'USER', description: 'The user you want to warn', required: true },
-				reason: { type: 'INTEGER', description: 'The reason for the warning; max 512 characters', required: false }
-			}
-		}, {
-			com: 'kick',
-			des: 'Used by command-specific role or admin to kick a member',
-			format: argEl('@user', 'user-tag or id') + argEl('?reason', 'text; max 512 characters'),
-			txt1: 'kick',
-			txt: 'kicked',
-			options: {
-				user: { type: 'USER', description: 'The user you want to kick', required: true },
-				reason: { type: 'INTEGER', description: 'The reason for the kick; max 512 characters', required: false }
-			}
-		}, {
-			com: 'ban',
-			des: 'Used by command-specific role or admin to ban a member',
-			format: argEl('@user', 'user-tag or id') + argEl('?reason', 'text; max 512 characters'),
-			txt1: 'ban',
-			txt: 'banned',
-			options: {
-				user: { type: 'USER', description: 'The user you want to ban', required: true },
-				reason: { type: 'INTEGER', description: 'The reason for the ban; max 512 characters', required: false },
-				duration: { type: 'STRING', description: `${durationDescription}; default: unlimited`, required: false }
-			}
-		}, {
-			com: 'unban',
-			des: 'Used by command-specific role or admin to unban a member',
-			format: argEl('@user', 'user-tag or id') + argEl('?reason', 'text; max 512 characters'),
-			txt1: 'unban',
-			txt: 'unbanned',
-			options: {
-				user: { type: 'USER', description: 'The user you want to unban', required: true },
-				reason: { type: 'INTEGER', description: 'The reason for the unban; max 512 characters', required: false }
-			}
-		}, {
-			com: 'mute',
-			des: 'Used by command-specific role or admin to mute a member',
-			format: argEl('@user', 'user-tag or id') + argEl('?reason', 'text; max 512 characters'),
-			txt1: 'mute',
-			txt: 'muted',
-			options: {
-				user: { type: 'USER', description: 'The user you want to mute', required: true },
-				reason: { type: 'INTEGER', description: 'The reason for the mute; max 512 characters', required: false },
-				duration: { type: 'STRING', description: `${durationDescription}; default: unlimited`, required: false }
-			}
-		}, {
-			com: 'unmute',
-			des: 'Used by command-specific role or admin to unmute a member',
-			format: argEl('@user', 'user-tag or id') + argEl('?reason', 'text; max 512 characters'),
-			txt1: 'unmute',
-			txt: 'unmuted',
-			options: {
-				user: { type: 'USER', description: 'The user you want to unmute', required: true },
-				reason: { type: 'INTEGER', description: 'The reason for the unmute; max 512 characters', required: false }
-			}
-		}
-	],
 	encodeT = n => Math.round((n || Date.now()) / 6e4 - 271e5), // Date ->  T
 	decodeT = (n, parse = false) => { //            T   -> Date
 		n = new Date((n + 271e5) * 6e4);
@@ -4257,14 +4096,14 @@ client.on('messageCreate', async m => { //Automod
 client.on('interactionCreate', async interaction => { // Slash-Commands
 	if (!(interaction.isCommand() && interaction.inCachedGuild())) return;
 
-	c = {
-		com: 'info role',
-		des: 'Used by moderator or admin to get info about a role',
-		format: argEl('@role', 'role-tag or id'),
-		options: {
-			role: { type: 'ROLE', description: 'The role you want the information of', required: true }
-		}
-	};
+	// c = {
+	// 	com: 'info role',
+	// 	des: 'Used by moderator or admin to get info about a role',
+	// 	format: argEl('@role', 'role-tag or id'),
+	// 	options: {
+	// 		role: { type: 'ROLE', description: 'The role you want the information of', required: true }
+	// 	}
+	// };
 
 
 	const subCommand = interaction.getSubcommand(false) || '',
