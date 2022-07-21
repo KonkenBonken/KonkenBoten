6a4b73ccc98d08ff41cff10ae02b285ba47d7328
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-21 13:16:21 +0200

    Added interface DataBaseData

--- database/DataBase.ts
+++ database/DataBase.ts
@@ -1,16 +1,24 @@
 import fs from 'fs';
+import { Snowflake } from './Utils';
 
 import { Guild } from './guild';
 import { Temp } from './temp';
 import { LoggedInUser } from './loggedInUser';
 
-class DataBase {
+interface DataBaseData {
+  guilds: Record<Snowflake, Guild>;
+  temp: Record<string, Temp>;
+  loggedIn: Record<Snowflake, LoggedInUser>;
+  voiceCreated: Snowflake[];
+}
+
+class DataBase implements DataBaseData {
   guilds: Record<Snowflake, Guild>;
   temp: Record<string, Temp>;
   loggedIn: Record<Snowflake, LoggedInUser>;
   voiceCreated: Snowflake[];
 
-  constructor(data: object) {
+  constructor(data: DataBaseData) {
     this.guilds = data.guilds;
     this.temp = data.temp;
     this.loggedIn = data.loggedIn;
@@ -22,5 +30,5 @@ const raw = JSON.parse(
   fs.readFileSync('DataBase.json', 'utf8')
     .trim()
     .replace(/Ã¤/g, 'ä').replace(/Ã¥/g, 'å').replace(/Ã¶/g, 'ö')
-)
+) as DataBaseData;
 export default new DataBase(raw);
