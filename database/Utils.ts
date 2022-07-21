ca864ff54774017e2eebb867ab3fba96bbffe8fb
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-21 13:17:20 +0200

    Removed outdated functions

--- database/Utils.ts
+++ database/Utils.ts
@@ -1,13 +1,3 @@
 export { Snowflake } from 'discord.js';
 
 export type EncodedT = number;
-
-export function mapValues<K, V, R>(obj: Record<K, V>, mapper: (V) => R): Record<K, R> {
-  for (const key in obj)
-    obj[key] = mapper(obj[key]);
-  return obj
-}
-
-export function mapToClass<C, K, V>(goalClass: C, obj: Record<K, V>) {
-  return mapValues(obj, (x: V) => new goalClass(x))
-}
