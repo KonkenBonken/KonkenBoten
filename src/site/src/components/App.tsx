f9d8d3be7858e3c93e88716ad7c2f8965c0444ac
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-22 01:11:17 +0200

    Structured files into directories

---
+++ src/site/src/components/App.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+    </Fragment>
+  );
+}
+
+export default App;
--- src/site/src/index.tsx
+++ src/site/src/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App.tsx';
+
+import './styles/index.scss';
+import App from './components/App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
