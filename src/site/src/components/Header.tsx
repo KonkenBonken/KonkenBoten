4818fc7b19c55e4a58c92e32715a94035c3fcc8f
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-22 22:26:39 +0200

    Added base styling

--- src/site/src/components/Header.tsx
+++ src/site/src/components/Header.tsx
@@ -4,7 +4,6 @@ export function Header() {
   return (
     <header>
       <Logo />
-      Header!
     </header>
   );
 }
--- src/site/src/components/Navigation.tsx
+++ src/site/src/components/Navigation.tsx
@@ -1,7 +1,6 @@
 export function Navigation() {
   return (
     <nav>
-      Nav!
     </nav>
   );
 }
