cb80d8ec32054b9a254534d7f2a64fd2bf1bae79
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-23 03:20:22 +0200

    Added user avatar

---
+++ src/site/src/components/DiscordImage.tsx
@@ -0,0 +1,7 @@
+export function DiscordImage({ src, srcSizes, className }: { src: string, srcSizes?: number[], className?: string }) {
+  const sizes = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+    .filter(size => !srcSizes || srcSizes.includes(size)),
+    srcset = (size: number) => `${src}?size=${size} ${size}w`;
+
+  return (<img src={`${src}?size=32`} srcSet={sizes.map(srcset).join()} className={className} />);
+}
--- src/site/src/components/Header.tsx
+++ src/site/src/components/Header.tsx
@@ -1,11 +1,17 @@
 import { Consumer } from '../context.tsx';
 import { ContextData } from '../types.d.ts';
 
+import { DiscordImage } from './DiscordImage.tsx';
+
 export function Header() {
   return (
     <header>
       <img className="logo" src="/icon.svg" alt="KonkenBoten's Logo" />
       <Consumer>
+        {({ user }: ContextData) => {
+          if (user)
+            return (<DiscordImage src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`} className="avatar" />)
+        }}
       </Consumer>
     </header>
   );
