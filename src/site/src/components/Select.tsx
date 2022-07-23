e5875b5c6963042eb867c647bb2811505814b960
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-23 21:25:00 +0200

    Added search in Select

--- src/site/src/components/Select.tsx
+++ src/site/src/components/Select.tsx
@@ -8,7 +8,8 @@ export function Select({ options, initialSelected, guildIcons = true, onChoice }
   const [open, setOpen] = useState(false),
     displayIcons = guildIcons && options.some(option => option.icon),
     [selectedId, setSelectedId] = useState(initialSelected),
-    selected = options.find(option => option.id == selectedId) || options[0];
+    selected = options.find(option => option.id == selectedId) || options[0],
+    [searchQuery, setSearchQuery] = useState('');
 
   if (!options.some(option => option.id == selectedId))
     setSelectedId(options[0].id)
@@ -18,23 +19,25 @@ export function Select({ options, initialSelected, guildIcons = true, onChoice }
       defaultValue={selected.label}
       onFocus={({ target }) => target.value = ''}
       onBlur={({ target }) => target.value = selected.label}
+      onChange={({ target }) => setSearchQuery(target.value)}
     />
-    {options.map(({ label, id, icon }) => (
-      <div key={id} id={id}
-        className={selectedId == id ? 'active' : ''}
-        onClick={({ target }) => {
-          setSelectedId(id);
-          onChoice && onChoice(id);
-        }}>
+    {options.filter(({ label, id }) => [label, id].some(text => text.includes(searchQuery)))
+      .map(({ label, id, icon }) => (
+        <div key={id} id={id}
+          className={selectedId == id ? 'active' : ''}
+          onClick={({ target }) => {
+            setSelectedId(id);
+            onChoice && onChoice(id);
+          }}>
 
-        {displayIcons && (icon ?
-          <DiscordImage src={`https://cdn.discordapp.com/icons/${id}/${icon}`}
-            srcSizes={[16, 32, 64]} /> :
-          <GuildPlaceholder name={label} />
-        )}
+          {displayIcons && (icon ?
+            <DiscordImage src={`https://cdn.discordapp.com/icons/${id}/${icon}`}
+              srcSizes={[16, 32, 64]} /> :
+            <GuildPlaceholder name={label} />
+          )}
 
-        <span>{label}</span>
-      </div>
-    ))}
+          <span>{label}</span>
+        </div>
+      ))}
   </div>)
 }
