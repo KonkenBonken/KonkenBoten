4cdd3f1f3cd8342f155e22ec8a1d69b09d436644
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-23 21:09:45 +0200

    Animate on hover

--- src/site/src/components/DiscordImage.tsx
+++ src/site/src/components/DiscordImage.tsx
@@ -1,7 +1,15 @@
-export function DiscordImage({ src, srcSizes, className }: { src: string, srcSizes?: number[], className?: string }) {
+import { useState } from 'react';
+
+export function DiscordImage({ src, srcSizes, className, animateOnHover }: { src: string, srcSizes?: number[], className?: string, animateOnHover?: false }) {
+  const [hovered, setHovered] = (animateOnHover ?? src.includes('a_')) ? useState(false) : [false, () => false];
+
   const sizes = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
     .filter(size => !srcSizes || srcSizes.includes(size)),
-    srcset = (size: number) => `${src}?size=${size} ${size}w`;
+    srcset = (size: number) => `${src}${hovered ? '.gif' : '.png'}?size=${size} ${size}w`;
 
-  return (<img src={`${src}?size=${sizes[0]}`} srcSet={sizes.map(srcset).join()} className={className} />);
+  return (<img
+    onMouseOver={() => setHovered(true)} onMouseOut={() => setHovered(false)}
+    src={`${src}.png?size=${sizes[0]}`} srcSet={sizes.map(srcset).join()} className={className}
+  />);
+
 }
