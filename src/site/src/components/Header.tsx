2f05121cc81dd5a1927fb5ee4d008a0f4efcce8c
KonkenBonken <konrad05pettersson@gmail.com>
2022-07-30 14:50:10 +0200

    Added context to ServerSelect

--- src/site/src/components/Header.tsx
+++ src/site/src/components/Header.tsx
@@ -2,14 +2,16 @@ import { DiscordImage } from './DiscordImage.tsx';
 import { ServerSelect } from './ServerSelect.tsx';
 import { ContextProps } from '../utils/context';
 
-export function Header({ context: { user } }: ContextProps) {
+export function Header({ context }: ContextProps) {
+  const { user } = context;
+
   return (
     <header>
       <img className="logo" src="/icon.svg" alt="KonkenBoten's Logo" />
       {user ?
         (<>
           <DiscordImage src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`} srcSizes={[16, 32, 64]} className="avatar" />
-          <ServerSelect servers={user.guilds} />
+          <ServerSelect servers={user.guilds} context={context} />
         </>) :
         (<a href="/oauth" class="login">
           <img src="/discord.svg" alt="Discord's Logo" />
