6043a4d7842a347ea6c858dbda3c215987481694
KonkenBonken <konrad05pettersson@gmail.com>
2022-08-14 02:24:35 +0200

    Fixed loading animation

--- src/site/src/components/Loading.tsx
+++ src/site/src/components/Loading.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from 'react';
 import { lazyWithPreload } from "react-lazy-with-preload";
+import { ReactComponent as LogoClip } from "../assets/logo-clip.svg";
 
 export function Loading() {
   return (
-    <div className="loading" >
-      {'abcde'.split('').map(letter => (
+    <div className="loading" style={{
+      clipPath: "url('#loading-clip-path')"
+    }}>
+      {'abcd'.split('').map(letter => (
         <div id={letter} key={letter} />
       ))}
+      <LogoClip />
     </div>
   );
 }
