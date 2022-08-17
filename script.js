0e2e5182db2713f04f8a2e25245f93d13c6e3e5f
KonkenBonken <konrad05pettersson@gmail.com>
2022-08-17 23:59:23 +0200

    Allow for undefined command settings
    Fixes #126

--- script.js
+++ script.js
@@ -3003,10 +3003,12 @@ io.on('connection', async socket => {
 							['tempmute', 'temporarily muted']
 						].forEach(([key, value]) => {
 							/* if coms default: delete*/
-							if (data.coms[key]?.txt == value) data.coms[key].txt = undefined;
-							else if (data.coms[key]?.txt) data.coms[key].txt = data.coms[key].txt.trim();
-							if (!+data.coms[key]?.role) data.coms[key].role = undefined;
-							if (!Object.values(data.coms[key]).filter(x => x).length) data.coms[key] = undefined;
+							if (data.coms[key]) {
+								if (data.coms[key].txt) data.coms[key].txt = data.coms[key].txt.trim();
+								if (data.coms[key].txt == value) data.coms[key].txt = undefined;
+								if (!+data.coms[key].role) data.coms[key].role = undefined;
+								if (!Object.values(data.coms[key]).filter(x => x).length) data.coms[key] = undefined;
+							}
 						});
 						['enabled', 'dmInvite', 'dmAll', 'tellWho', 'logsEnabled', 'banMessage', 'timeout'].forEach(key => {
 							data[key] = data[key] ? 1 : undefined; /* if falsy: delete*/
