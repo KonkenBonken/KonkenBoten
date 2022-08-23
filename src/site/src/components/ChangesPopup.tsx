5ae2e0455bb154d03bdfefd82659307c65eb578e
KonkenBonken <konrad05pettersson@gmail.com>
2022-08-23 19:01:51 +0200

    Added button

--- src/site/src/components/ChangesPopup.tsx
+++ src/site/src/components/ChangesPopup.tsx
@@ -1,8 +1,10 @@
 import { ContextProps } from '../utils/context';
+import { saveChanges } from '../utils/socket.ts';
 
 export default function ChangesPopup({ changes }: ContextProps) {
 
   return (<section id="changes-popup">
     You have {changes.length} unsaved change{changes.length > 1 && 's'}
+    <button onClick={saveChanges}>Save</button>
   </section>);
 }
