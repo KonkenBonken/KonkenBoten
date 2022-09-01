d48a231b6d4487d90de864bf7ae22b0406e15e99
KonkenBonken <konrad05pettersson@gmail.com>
2022-09-01 19:49:05 +0200

    Changed mobile aspect ratio

--- src/site/src/utils/utils.ts
+++ src/site/src/utils/utils.ts
@@ -4,4 +4,4 @@ export function titleCase(string: string) {
   return string.replace(/^_*(.)|_+(.)/g, (s, c, d) => c ? c.toUpperCase() : ' ' + d.toUpperCase())
 }
 
-export const isMobile = () => useMediaQuery({ query: '(max-aspect-ratio: 3/4)' });
+export const isMobile = () => useMediaQuery({ maxAspectRatio: '3/5' });
