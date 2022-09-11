c04e69b527f7203ae1030462082b8d9fb8a24731
KonkenBonken <konrad05pettersson@gmail.com>
2022-09-11 15:14:58 +0200

    Added Guild Selector route

--- src/site/src/route/GuildSelector.tsx
+++ src/site/src/route/GuildSelector.tsx
@@ -1,5 +1,21 @@
-export default function GuildSelector() {
-  return (
-    <></>
-  );
+import { NavLink } from "react-router-dom";
+
+import { DiscordImage, GuildPlaceholder } from '../components/DiscordImage.tsx';
+import { ContextProps } from '../../../../types/context';
+
+import '../styles/routes/guild-selector.scss';
+
+export default function GuildSelector({ context: { user: { guilds } } }: ContextProps) {
+  return <section id="selector">
+    {guilds.map(({ id, icon, name }) =>
+      <section>
+        {icon ?
+          <DiscordImage src={`https://cdn.discordapp.com/icons/${id}/${icon}`} srcSizes={[64, 128, 256]} /> :
+          <GuildPlaceholder name={name} />
+        }
+        <h3>{name}</h3>
+        <NavLink to={id}>Go</NavLink>
+      </section>
+    )}
+  </section>;
 }
