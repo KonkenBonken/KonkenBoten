f05182782fffeafaf580d609837bba7a8da0788a
KonkenBonken <konrad05pettersson@gmail.com>
2022-09-13 19:39:08 +0200

    Added Select Server button to home page

--- src/site/src/route/Home.tsx
+++ src/site/src/route/Home.tsx
@@ -1,9 +1,14 @@
+import { NavLink } from "react-router-dom";
 import { Title } from '../components/Title.tsx';
+import { ContextProps } from '../../../../types/context';
 
 import '../styles/routes/home.scss';
 
-export default function Home() {
+export default function Home({ context: { user } }: ContextProps) {
   return (<>
     <Title />
+    {user && <NavLink id="select-server" to="Guild">
+      Select Server
+    </NavLink>}
   </>);
 }
